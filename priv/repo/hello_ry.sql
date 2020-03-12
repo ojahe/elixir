0bd8b1a98a0fba19e3b0caa72d2c057448349ea8
hhp <huaping.he@changhong.com>
2020-03-12 13:58:12 +0800

    添加数据库脚本

--- priv/repo/hello_ry.sql
+++ priv/repo/hello_ry.sql
@@ -15,9 +15,9 @@
 */
 
 SET NAMES utf8mb4;
-SET FOREIGN_KEY_CHECKS = 0;
+SET FOREIGN_KEY_CHECKS = 0; 
 
--- ----------------------------
+-- ------------------------------
 -- Table structure for gen_table
 -- ----------------------------
 DROP TABLE IF EXISTS `gen_table`;
